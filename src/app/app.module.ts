a811e03915ddd091925fd589f00f0b3a6bed282e
Mark Qian <markqian@Marks-MacBook-Pro.local>
2021-02-22 14:43:46 -0500

    first upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {Test01Component} from './test01/test01.component';
+import {Test02Component} from './test02/test02.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Test03Component } from './test03/test03.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Test01Component,
+    Test02Component,
+    Test03Component,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
